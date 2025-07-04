b0af2a540726f1ed69669a584619940e93ff9372
rutu-sh <ruturajs10@hotmail.com>
2025-07-04 02:04:09 -0400

    fix: smal fix

--- src/main.rs
+++ src/main.rs
@@ -31,8 +31,11 @@ fn main() {
     let vctr: Vec<f32> = vec![5.0; 1000000];
     let vctr2: Vec<f32> = vec![6.0; 1000000];
 
+    let v1 = VectorFloat32::new(&vctr);
+    let v2 = VectorFloat32::new(&vctr2);
+
     let start_time = Instant::now();
-    let res = similarity::cosine_sim(&vctr, &vctr2);
+    let res = similarity::cosine_sim(&v1.unit_vec, &v2.unit_vec);
     println!("res = {}", res);
     let duration = start_time.elapsed();
 
